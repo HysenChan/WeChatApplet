9b57cbe681c0f243a1f10e84a956e3cf9c13d883
HysenChan <HysenChan@163.com>
2024-05-11 12:00:50 +0800

    获取用户个人信息

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,12 +1,11 @@
 Page({
-  data: { items: [] },
-  onLoad() {
-    const that = this;
-    wx.request({
-      url: 'http://localhost:3000/items',
-      success(res) {
-        that.setData({ items: res.data });
-      }
-    })
+  data: {
+    name: ''
+  },
+  buttonHandler(event) {
+    if (!event.detail.userInfo) return;
+    this.setData({
+      name: event.detail.userInfo.nickName
+    });
   }
-})
+});
